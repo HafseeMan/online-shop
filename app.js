f0728640e9b0552c2cd222c5a48360ebed08a8bc
HafseeMan <hafoosah1@gmail.com>
2020-05-09 18:41:30 +0100

    updating functions

--- app.js
+++ app.js
@@ -9,19 +9,47 @@ function item(name, price, amount){
 function UI() {}
 
 //Add item to cart
-UI.prototype.addToCart = function(item){
-    const list = document.getElementById('shop-list');
-    //create tr element
-    const row = document.createElement('tr');
-    //insert cols
-    row.innerHTML = `
-    <td>${item.name}</td>
-    <td>${item.amount}</td>
-    <td>${item.price}</td>
-    <td><a href="#" class="delete">X<a></td>
-  `;
-  list.appendChild(row);
+UI.prototype.addToCart = function(target){
+   if(target.className === 'addBtn'){
+    var card = Boo; //event.target.parentNode;
+    var title = Boo; //card.getElementsByClassName('itemName')[0].innerText;
+    var price = 1000;
+    var imgSrc = '/imgs/item1.jpg';
+
+    console.log('butt');
+    //addItemToCart(imgSrc, title, price)
+
+   }
 }
+/*
+   //find the dress object in the array of dress objects and display properties in cart
+    function returnDress(dress) {
+        return  dress.name === title;
+    }
+    console.log(dresses.find(returnDress)); //undefined??? why?
+
+    var dress = dresses.find(returnDress);
+    addItemToCart(dress.img, dress.name, dress.price);
+*/
+
+
+   // updateCartTotal()
+
+function addItemToCart(img, title, price){
+    var list = document.getElementById('shop-list');
+    var newCartItem = `
+        <tr> 
+            <td><img src='"${img}"' class='cart-img'></td>
+            <td>"${title}"</td>
+            <td><input value= '1' class= 'quantity-input' type='number'></td>
+            <td>"${price}"</td>
+            <td><a href="#" class="delete">X<a></td>
+    
+            `
+
+    list.innerHTML+= newCartItem
+}
+
 
 //Delete function
 UI.prototype.deleteItem = function(target){
@@ -30,33 +58,67 @@ UI.prototype.deleteItem = function(target){
         target.parentElement.parentElement.remove();
     }
 }
+//Quantity changed function
+UI.prototype.quantityChange = function(target){
+    console.log('changed');
+}
+//Array of shopping items
+const dresses = [
+    {name:'Dress 1', price: 2500, img: '/imgs/item1.jpg'},
+    {name:'Dress 2', price: 7500, img: '/imgs/item2.jpg'},
+    {name:'Dress 3', price: 2500, img: '/imgs/item3.jpg'},
+    {name:'Dress 4', price: 5000, img: '/imgs/item4.jpg'},
 
-//Array of objects
- const shopItems = [{name: 'item 1', price: 2500 , amount: 1},{name: 'item 2', price: 5000 , amount: 1},{name: 'item 3', price: 2500 , amount: 1}]
+];
+ 
+ //UI for showing and exiting from cart
+    var cart = document.getElementById('cart');
+    var showBtn = document.getElementById('showCart');
+    var exitBtn = document.getElementById('exitCart');
+    var content = document.getElementById('content');//rest of container
 
- //grabing elements
- var cart = document.getElementById('cart');
- var showBtn = document.getElementById('showCart');
- var exitBtn = document.getElementById('exitCart');
- var content = document.getElementById('content');//rest of container
+    //.. show cart btn
+    function showCart(){   
+        cart.style.display='block';
+        exitBtn.style.display='block';
+        showBtn.style.display='none'; 
+        content.style.display='none';
+    }
+    //... For exit btn
+    function exitCart(){
+        exitBtn.style.display='none';    
+        cart.style.display='none';
+        showBtn.style.display='block'; 
+        content.style.display='block';
+    }
 
- //Event Listener for show cart btn
- function showCart(){   
-    cart.style.display='block';
-    exitBtn.style.display='block';
-    showBtn.style.display='none'; 
-    content.style.display='none';
- }
- //... For exit btn
- function exitCart(){
-    exitBtn.style.display='none';    
-    cart.style.display='none';
-    showBtn.style.display='block'; 
-    content.style.display='block';
- }
 //Event Listener for add to cart
+ //   var addBtns = document.getElementsByClassName('addBtns');
+ 
+ document.getElementById('content').addEventListener('click', function(e){
 
-//WHEN "ADD CART BTN" IS CLICKED.. ADD THE NAME AND PRICE OF IT'S OBJECT TO TABLE IN THE CART.
+    const ui = UI();
+    ui.addToCart(e.target);
+
+ });
 
 
 
+
+
+// Event Listener for delete
+document.getElementById('shop-list').addEventListener('click', function(e){
+
+    // Instantiate UI
+    const ui = new UI();
+  
+    // Delete book
+    ui.deleteItem(e.target);
+    e.preventDefault();
+  });
+//Event listener for quantity change
+document.getElementById('shop-list').addEventListener('change', function(e){
+    const ui = new UI();
+
+    ui.quantityChange(e.target);
+})
